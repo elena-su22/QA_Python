cd8ebc5b68686aab0f838129a02bd6739f53783e
elena-su22 <167975416+elena-su22@users.noreply.github.com>
2024-05-23 15:29:20 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,9 +5,6 @@ screenv = pygame.display.set_mode((960,540))
 x = True
 while x == True:
     pygame.display.update()
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_UP:
-          vel_y = vel_y - 10
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             x = False
