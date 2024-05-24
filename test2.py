07ddeec3a91ca40c39041d72353926ffb6f3c224
elena-su22 <167975416+elena-su22@users.noreply.github.com>
2024-05-24 15:45:13 +0300

    lesson3

--- test2.py
+++ test2.py
@@ -7,16 +7,16 @@ iconv = pygame.image.load('C:\\Users\\AttekPC\\Desktop\\game\\imageforstart\\rup
 pygame.display.set_icon(iconv)
 color1 = (180, 150, 250)
 square1 = pygame.Surface((175,50))
-square1.fill((25, 25, 112))
+square1.fill((192, 192, 192))
 font1 = pygame.font.Font(None,40)
-text1 = font1.render('Надпись', False,'White')
+text1 = font1.render('Надпись', False,(70, 70, 70))
 object1 = pygame.image.load('C:\\Users\\AttekPC\\Desktop\\game\\imageforstart\\rupee_icon.png')
 x = True
 while x == True:
     pygame.display.update()
     screenv.fill(color1)
     screenv.blit(square1, (50,50))
-    pygame.draw.rect(screenv,(25, 25, 112),(50,150,175,50)) ## pygame.draw.форма(rect-прямоугольник)(где-экран,(код цвета), (х(растояние от вертикальной границы экрана слева), (х(растояние от горизонтальной границы экрана сверху), х(размер вертикально),х(размер горизонтально))))
+    pygame.draw.rect(screenv,(192, 192, 192),(50,150,175,50)) ## pygame.draw.форма(rect-прямоугольник)(где-экран,(код цвета), (х(растояние от вертикальной границы экрана слева), (х(растояние от горизонтальной границы экрана сверху), х(размер вертикально),х(размер горизонтально))))
     #screenv.blit(text1,(50,50))
     square1.blit(text1,(0,0)) ## то же что в предыдущей строке, но там на экране, а тут на квадрате
     screenv.blit(object1,(250,10))
